b1b2fc477be75497e501503bf3a93c4d5c272727
hamuse <hamuseco@gmail.com>
2019-12-18 12:22:29 +0900

    first commit

---
+++ src/main/java/com/hamuse/web/HamuseSbvApplication.java
@@ -0,0 +1,13 @@
+package com.hamuse.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamuseSbvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamuseSbvApplication.class, args);
+	}
+
+}
